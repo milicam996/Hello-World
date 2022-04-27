c5f803870eabb14138163ae039e04f3d0c360885
Milica Musulin <musulinmilica@gmail.com>
2022-04-27 22:29:48 +0200

    Assigment 3

--- src/main/java/com/assignment/HelloWorld/controller/HelloWorldController.java
+++ src/main/java/com/assignment/HelloWorld/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.assignment.HelloWorld.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,11 @@ public class HelloWorldController {
     public String HelloRest() {
         return "Hello World";
     }
+
+    @GetMapping(value = "hello-world", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String helloWorldHTML() {
+        return "<html>\n <header><title>Hello World</title></header>\n <body>\n Hello world\n </body>\n </html>";
+    }
 }
+
