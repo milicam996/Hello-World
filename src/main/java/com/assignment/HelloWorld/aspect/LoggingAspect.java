22ec5fcbaeae18aa322859917261428ebd0e7bf5
Milica Musulin <musulinmilica@gmail.com>
2022-05-04 16:57:33 +0200

    Assigment 7

---
+++ src/main/java/com/assignment/HelloWorld/aspect/LoggingAspect.java
@@ -0,0 +1,36 @@
+package com.assignment.HelloWorld.aspect;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger LOGGER = LogManager.getLogger(LoggingAspect.class);
+
+    @Around("execution(* com.assignment.HelloWorld..*(..)))")
+    public Object logMethodExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        final StopWatch stopWatch = new StopWatch();
+
+        //calculate method execution time
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+
+        //Log method execution time
+        LOGGER.info("Hello World Asssigment - AOP - Execution time of "
+                + methodSignature.getDeclaringType().getSimpleName() // Class Name
+                + "." + methodSignature.getName() + " " // Method Name
+                + ":: " + stopWatch.getTotalTimeMillis() + " ms");
+
+        return result;
+    }
+}
--- src/main/java/com/assignment/HelloWorld/service/HelloWorldService.java
+++ src/main/java/com/assignment/HelloWorld/service/HelloWorldService.java
@@ -2,6 +2,8 @@ package com.assignment.HelloWorld.service;
 
 import com.assignment.HelloWorld.model.HelloWorldModel;
 import com.assignment.HelloWorld.repository.HelloWorldRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,16 @@ public class HelloWorldService {
     @Autowired
     HelloWorldRepository helloWorldRepository;
 
+    private static final Logger LOGGER = LogManager.getLogger(HelloWorldService.class);
+
     public String GetByLang(String lang) {
-        HelloWorldModel helloWorldModel = helloWorldRepository.GetByLang(lang);
+        HelloWorldModel helloWorldModel=null;
+        try {
+          helloWorldModel = helloWorldRepository.GetByLang(lang);
+        } catch (Exception exception) {
+            LOGGER.error("Error occurred in business service : {}",exception.getMessage());
+        }
+
         return helloWorldModel.getHw_translation();
     }
 
