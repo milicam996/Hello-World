c83eb8b693ab145382d08a0f08ab26c9e5729b94
Milica Musulin <musulinmilica@gmail.com>
2022-05-04 22:01:14 +0200

    Assigment 10

--- src/main/java/com/assignment/HelloWorld/service/HelloWorldService.java
+++ src/main/java/com/assignment/HelloWorld/service/HelloWorldService.java
@@ -2,31 +2,65 @@ package com.assignment.HelloWorld.service;
 
 import com.assignment.HelloWorld.model.HelloWorldModel;
 import com.assignment.HelloWorld.repository.HelloWorldRepository;
+import org.apache.coyote.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Service
 public class HelloWorldService {
     @Autowired
     HelloWorldRepository helloWorldRepository;
 
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
     private static final Logger LOGGER = LogManager.getLogger(HelloWorldService.class);
 
     public String GetByLang(String lang) {
-        HelloWorldModel helloWorldModel = new HelloWorldModel();
-        try {
-            helloWorldModel = helloWorldRepository.GetByLang(lang);
-        } catch (Exception exception) {
-            LOGGER.error("Error occurred in business service : {}", exception.getMessage());
+        if (activeProfile.equals("external")) {
+            return GetByLangAPI(lang);
+        } else {
+            HelloWorldModel helloWorldModel = new HelloWorldModel();
+            try {
+                helloWorldModel = helloWorldRepository.GetByLang(lang);
+            } catch (Exception exception) {
+                LOGGER.error("Error occurred in hello world service : {}", exception.getMessage());
+            }
+            return helloWorldModel.getHw_translation();
         }
-
-        return helloWorldModel.getHw_translation();
     }
-
 
     public void saveOrUpdate(HelloWorldModel language) {
         helloWorldRepository.save(language);
     }
+
+    private String GetByLangAPI(String lang) {
+        HttpResponse<String> response = null;
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=" + lang + "&api-version=3.0&from=en&profanityAction=NoAction&textType=plain"))
+                    .header("content-type", "application/json")
+                    .header("X-RapidAPI-Host", "microsoft-translator-text.p.rapidapi.com")
+                    .header("X-RapidAPI-Key", "7c486caf66msh7bec070c1113d78p1ea0f7jsne1436838fe73")
+                    .method("POST", HttpRequest.BodyPublishers.ofString("[\r\n    {\r\n        \"Text\": \"Hello World.\"\r\n    }\r\n]"))
+                    .build();
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (Exception exception) {
+            LOGGER.error("Error occurred in hello world service : {}", exception.getMessage());
+        }
+
+        return response.body();
+
+    }
+
+
 }
